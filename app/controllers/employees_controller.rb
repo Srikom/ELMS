8e80e19a81236d83ba874ff148bac23cea7437ef
Yung <wyungz7@gmail.com>
2013-04-29 12:05:43 +0800

    path

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -29,7 +29,7 @@ class EmployeesController < ApplicationController
     @employees.destroy 
 
     flash[:notice] = "User has been deleted"
-    redirect_to employees_index_path
+    redirect_to employees_path
   end
   
 end
