b1dcfaa6b1510e23667f9c715f6ab697075b75c5
Yung <wyungz7@gmail.com>
2013-04-30 08:24:57 +0800

    emp controller.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -4,16 +4,24 @@ class EmployeesController < ApplicationController
     @employees = Employee.all
   end
 
+  def destroy
+    @employees = Employee.find(params[:id])
+    @employees.destroy 
+
+    flash[:notice] = "User has been deleted"
+    redirect_to employees_path
+  end
+
+  def show
+    @employees = Employee.find(params[:id])
+  end
+
   def new
 
   end
 
   def create
 
-  end
-
-  def show
-    @employees = Employee.find(params[:id])
   end
 
   def edit
@@ -24,12 +32,5 @@ class EmployeesController < ApplicationController
 
   end
 
-  def destroy
-    @employees = Employee.find(params[:id])
-    @employees.destroy 
-
-    flash[:notice] = "User has been deleted"
-    redirect_to employees_path
-  end
   
 end
