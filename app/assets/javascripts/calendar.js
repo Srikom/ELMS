22392aff4528597842e097b075d112755e5142ea
karenkhong <myreal.ksk@gmail.com>
2013-05-02 16:50:13 +0800

    show button panel in calendar

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -6,7 +6,8 @@ $(function(){
 	changeYear: true,
 	duration:"fast",
 	stepMonths:0,
-	minDate: 0 
+	minDate: 0 ,
+	showButtonPanel:true
 			});
 });
 $(function(){
@@ -17,7 +18,7 @@ $("#leave_application_end_date").datepicker({
 	duration:"fast",
 	stepMonths:0,
 	minDate: 0 
-	
+	showButtonPanel:true
 			});
 });
  
